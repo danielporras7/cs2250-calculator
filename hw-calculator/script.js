5b1c17b382edc2f1fe77cd87090b720db8be6d0a
Daniel Porras <75912576+danielporras7@users.noreply.github.com>
2023-10-02 07:49:28 -0700

    Add files via upload

--- hw-calculator/script.js
+++ hw-calculator/script.js
@@ -16,11 +16,31 @@ function clearDisplay() {
 
 function calculateResult() {
   if (currentInput === "") return;
-  try {
-    result = eval(currentInput);
-    document.getElementById("display").value = result;
-    currentInput = result.toString();
-  } catch (error) {
+  const operands = currentInput.split(/(\+|-|\*|\/)/);
+  if (operands.length !== 3) {
     document.getElementById("display").value = "Error";
+    return;
   }
+  const num1 = parseFloat(operands[0]);
+  const num2 = parseFloat(operands[2]);
+  switch (operands[1]) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 === 0) {
+        document.getElementById("display").value = "Error";
+        return;
+      }
+      result = num1 / num2;
+      break;
+  }
+  document.getElementById("display").value = result;
+  currentInput = result.toString();
 }
